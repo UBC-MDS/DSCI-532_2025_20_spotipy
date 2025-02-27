a2add8e9efd56d3b15aafc0f9ca4ff5dc074f0f9
Brian Chang <32214220+BChangs99@users.noreply.github.com>
2025-02-26 17:29:41 -0800

    Add scatterplot (#52)
    * Add danceability vs. popularity scatterplot with callbacks
    
    * Assign scatterplot to a component

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 from dash import Dash, html, dcc, Input, Output, callback
 import dash_bootstrap_components as dbc
+import dash_vega_components as dvc
+import altair as alt
 import pandas as pd
 
+# Set Altair theme to dark
+alt.theme.enable('carbong90')
 
 # Intialization
 app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
@@ -25,18 +29,20 @@ data.columns = ['title',
               'speechiness', 
               'popularity']
 
-# Create a list of HTML elements to display the top 6 artists
-years = sorted(data['year'].unique())
-default_year = 2010
 
-# Components
-
+# Components -----------------
 # Top artists chart
 artist_elements = html.Div(id='artist-list')
 
-# Selectors
+# Scatterplot
+scatterplot = dvc.Vega(id='scatterplot', spec={}, style={'width': '100%', 'marginTop': '1vh'})
 
+# Selectors ------------------
 # Year Selector for Top Artists
+
+years = sorted(data['year'].unique())
+default_year = 2010
+
 year_dropdown = html.Div([
     html.H3("Year"),
     dcc.Slider(
@@ -57,8 +63,8 @@ song_duration = html.Div([
     dcc.Input(id="duration_max", type="number", placeholder="Max Duration", debounce=True, value=300)
 ])
 
-# Callbacks
-
+# Callbacks ----------
+# Artist List Callback 
 @callback(
     Output('artist-list', 'children'),
     Input('year-dropdown', 'value'),
@@ -89,6 +95,46 @@ def update_artist_list(selected_year, selected_duration_min, selected_duration_m
         ], style={'fontSize': '1.5rem'})
     ])
 
+# Scatterplot Callback
+@callback(
+    Output('scatterplot', 'spec'),
+    Input('year-dropdown', 'value'),
+    Input('duration_min', 'value'),
+    Input('duration_max', 'value')
+)
+def update_scatterplot(selected_year, selected_duration_min, selected_duration_max):
+    # Apply filters on data
+    filtered_data = data[
+        (data['year'] == selected_year) &
+        (data['duration'] >= selected_duration_min) &
+        (data['duration'] <= selected_duration_max)
+    ]
+
+    return (
+        alt.Chart(
+            filtered_data, width='container'
+        ).mark_point(filled=True, size=100).encode(
+            x=alt.X('popularity', title='Popularity', scale=alt.Scale(zero=False)),
+            y=alt.Y('danceability', title='Danceability', scale=alt.Scale(zero=False)),
+            tooltip=[
+                alt.Tooltip('title', title='Song Title'), 
+                alt.Tooltip('artist', title='Artist'), 
+                alt.Tooltip('popularity', title='Popularity'), 
+                alt.Tooltip('danceability', title='Danceability'),
+                alt.Tooltip('duration', title='Duration (Seconds)')
+            ]
+        ).properties(
+            title=f'Danceability vs. Popularity in {selected_year}'
+        ).configure_axis(
+            labelFontSize=16,
+            titleFontSize=20
+        ).configure_title(
+            fontSize=20
+        ).to_dict()
+    )
+
+    
+# App --------------
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
@@ -100,8 +146,8 @@ app.layout = dbc.Container([
             html.H3("Widget4")
         ], width=4, style={'borderRight': 'solid 3px'}),
         dbc.Col([
+            scatterplot,
             artist_elements,
-            html.H3("Chart2"),
             html.H3("Chart3"),
             html.H3("Chart4")
         ], width=8)
